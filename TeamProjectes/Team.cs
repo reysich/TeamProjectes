e11d406f4cb484ed3c183c3af9e20d9a7298bbe4
Denchik-koss <kustikov18@gmail.com>
2024-12-07 14:24:18 +0300

    Add class Team2

--- TeamProjectes/Team.cs
+++ TeamProjectes/Team.cs
@@ -1,25 +1,49 @@
 ﻿using System;
 
-public class Team
+public class Team : INameAndCopy
 {
     protected string nameOrganiz;
 	protected int registrId;
 	public string NameOrganiz
 	{
-		set { nameOrganiz = value; }
+		set 
+		{
+			if (value == "")
+			{
+                throw new Exception("Пустая строка",value);
+            }
+			else
+			{
+                nameOrganiz = value;
+            }
+		}
 		get { return nameOrgani; }
 	}
 	public int RegistrId
 	{
-		set { registrId = value; }
+		set 
+		{
+			if(value < 0)
+			{
+				throw new ArgumentException("Реистрационный номер не может быть 0 или меньше",value);
+			}
+			else
+			{
+                registrId = value;
+            }
+		}
 		get { return registrId; }
 	}
     public Team()
 	{
-	}
+		registrId = 0;
+		NameOrganiz = "default";
+    }
 	public Team (string nameOrganiz , int regisrtId)
 	{
 		this.nameOrganiz = nameOrganiz;
 		this.registrId = regisrtId;
 	}
+
+
 }

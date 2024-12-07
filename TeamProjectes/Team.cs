46022deff03a120ac39b820213f6f707364fbac0
Denchik-koss <kustikov18@gmail.com>
2024-12-07 14:10:44 +0300

    Add class Team

---
+++ TeamProjectes/Team.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Team
+{
+    protected string nameOrganiz;
+	protected int registrId;
+	public string NameOrganiz
+	{
+		set { nameOrganiz = value; }
+		get { return nameOrgani; }
+	}
+	public int RegistrId
+	{
+		set { registrId = value; }
+		get { return registrId; }
+	}
+    public Team()
+	{
+	}
+	public Team (string nameOrganiz , int regisrtId)
+	{
+		this.nameOrganiz = nameOrganiz;
+		this.registrId = regisrtId;
+	}
+}
